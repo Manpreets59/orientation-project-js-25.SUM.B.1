2e12a858382f3d53c285a4a25863cefa3fc3368b
Hao Ye <33074691+yehao09@users.noreply.github.com>
2025-06-20 18:32:14 -0400

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
+test("renders learn react link", () => {
 test("renders Resume Builder heading", () => {
   render(<App />);
-
-  const headingElement = screen.getByText(/resume builder/i);
-  expect(headingElement).toBeInTheDocument();
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
